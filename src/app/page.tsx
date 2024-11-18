19e76d212b6e2766c7291ca3198f5162f610ed84
AymanWahman <aymanwahman80@gmail.com>
2024-11-18 15:34:02 +0200

    size video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,92 +1,61 @@
-'use client';
+"use client";
 
-import Image from "next/image";
+// import Image from "next/image";
+import DataNews from "../components/DataNews";
+import NewsCard from "../components/NewsCard";
 
 export default function Home() {
   return (
     <div className="mt-14 md:mt-16 shadow-xl">
-      
       {/* قسم الفيديو الرئيسي */}
+
       <section className="mb-5 p-5 mx-auto rounded-lg">
-  <div className="grid md:grid-cols-2 place-items-center">
-    <div className="text-center">
-      <h1 className="text-2xl md:text-3xl font-bold mb-4 text-seaBlue">في بورسعيد</h1>
-      {/* تضمين الفيديو مع ضبط المقاسات */}
-      <div className="relative w-full h-[250px] md:h-[300px] rounded-lg overflow-hidden shadow-2xl border-4 border-gray-500">
-        <video
-          className="w-full h-full object-cover"
-          controls
-          preload="metadata"
-          src="/videos/portsaidin2.mp4"
-        >
-          متصفحك لا يدعم تشغيل الفيديو.
-        </video>
-      </div>
-      <p className="mb-4 mt-2 text-gray-500 dark:text-seagullGray">
-        اكتشف جمال بورسعيد من خلال مناظرها الخلابة
-      </p>
-    </div>
-
-    <div dir="rtl" className="px-7 space-y-5">
-      <p className="text-gray-600">
-        بورسعيد هي مدينة ساحلية مصرية تقع علي المدخل الشمالي لقناة السويس.
-      </p>
-      <p className="text-gray-600">
-        تأسست بورسعيد عام 1859م مع بدء أعمال حفر قناة السويس، وقد أنشئت خصيصا لتكون ميناء ومقرا للعاملين في مشروع القناة.
-      </p>
-      <p className="text-gray-600">
-        سميت المدينة بهذا الأسم نسبة إلي الخديوي سعيد باشا، الذي حكم مصر في ذلك الوقت ووافق علي حفر قناة السويس.
-      </p>
-    </div>
-  </div>
-</section>
-
-
-      {/* باقي محتوى الصفحة */}
-      <section className="pb-6 px-4">
-        <h2 className="text-2xl font-bold text-center">أحدث الأخبار</h2>
-        <div className="mt-3 grid grid-cols-1 md:grid-cols-3 gap-4 ">
-          {/* بطاقات الأخبار */}
-          <div className="">
-          <Image
-           className="rounded-xl border-2 border-gray-600 shadow-xl w-full h-[250px]"
-           src="/imgs/news1.jpg"
-           alt="Portsaidin"
-           width={250}
-           height={220}
-           priority
-         />
-         <p className="text-center p-2">انتعاش في السياحة العالمية: شهدت بورسعيد عودة قوية للسياحة، حيث استقبلت أكثر من 10,000 سائح خلال شهر واحد. تضمنت الفعاليات أجواء احتفالية في ساحة مصر واستقبال السفن السياحية بعروض السمسمية الشهيرة​
- </p>
+        <div className="grid md:grid-cols-2 place-items-center">
+          <div className="text-center">
+            <h1 className="text-2xl md:text-3xl font-bold mb-4 text-seaBlue">
+              في بورسعيد
+            </h1>
+            {/* تضمين الفيديو مع ضبط المقاسات */}
+            <div className="relative w-full h-[250px] md:h-[300px] rounded-lg overflow-hidden shadow-2xl border-4 border-gray-500">
+              <video
+                className="w-full h-full object-cover"
+                autoPlay
+                preload="metadata"
+                src="/videos/portsaidin2.mp4"
+              >
+                متصفحك لا يدعم تشغيل الفيديو.
+              </video>
+            </div>
+            <p className="mb-4 mt-2 text-gray-500 dark:text-seagullGray">
+              اكتشف جمال بورسعيد من خلال مناظرها الخلابة
+            </p>
           </div>
-          <div className="">
-          <Image
-           className="rounded-xl border-2 border-gray-600 shadow-xl w-full h-[250px]"
-           src="/imgs/news3.jpg"
-           alt="Portsaidin"
-           width={250}
-           height={220}
-           priority
-         />
-         <p className="text-center p-2">يشهد ميناء بورسعيد السياحي، فجر غد الثلاثاء، استقبال السفينة السياحية الفاخرة (LIRICA 2) ضمن رحلات سياحة اليوم الواحد، حيث وجه المحافظ اللواء محب حبشي، بتوفير كافة الخدمات والترتيبات اللازمة لاستقبال الضيوف الكرام، وذلك في إطار حرص المحافظة على تعزيز السياحة ورفع شأن مصر</p>
-          </div>
-          <div className="">
-          <Image
-           className="rounded-xl border-2 border-gray-600 shadow-xl w-full h-[250px]"
-           src="/imgs/news2.jpg"
-           alt="Portsaidin"
-           width={250}
-           height={220}
-           priority
-         />
-         <p className="text-center p-2">ميناء بورسعيد السياحى يستقبل 4 سفن سياحية ويختا فى 20 يوما.. الأهالى: بشائر الخير هلت علينا.. حمام السلام يستقبل الزوار فى ساحة مصر.. ومسئولو السياحة يقدمون الورود لأكثر من 5 آلاف سائح خلال الشهر الحالى</p>
+
+          <div dir="rtl" className="px-7 space-y-5">
+            <p className="text-gray-600">
+              بورسعيد هي مدينة ساحلية مصرية تقع علي المدخل الشمالي لقناة السويس.
+            </p>
+            <p className="text-gray-600">
+              تأسست بورسعيد عام 1859م مع بدء أعمال حفر قناة السويس، وقد أنشئت
+              خصيصا لتكون ميناء ومقرا للعاملين في مشروع القناة.
+            </p>
+            <p className="text-gray-600">
+              سميت المدينة بهذا الأسم نسبة إلي الخديوي سعيد باشا، الذي حكم مصر
+              في ذلك الوقت ووافق علي حفر قناة السويس.
+            </p>
           </div>
         </div>
       </section>
-    
+
+      {/* قسم الأخبار */}
+      <section className="pb-6 px-4">
+        <h2 className="text-2xl font-bold text-center">أحدث الأخبار</h2>
+        <div className="mt-3 ">
+          {DataNews.map((newsItem) => (
+            <NewsCard key={newsItem.id} news={newsItem} />
+          ))}
+        </div>
+      </section>
     </div>
   );
 }
-
-
-
--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -1,4 +1,4 @@
-// components/LetterCard.tsx
+// components/NewsCard.tsx
 
 import Image from 'next/image';
 
@@ -10,32 +10,27 @@ interface News {
 }
 
 interface NewsCardProps {
-  news: News; // الحقل letter من النوع Letter
-  readText?: () => void; // تمرير دالة readText
+  news: News; 
+
 }
 
-const NewsCard: React.FC<NewsCardProps> = ({ news, readText }) => {
+const NewsCard: React.FC<NewsCardProps> = ({ news }) => {
   return (
-    <article className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center p-4 rounded shadow-md hover:shadow-lg transition-shadow duration-300">
+    <article className="grid md:grid-cols-3 gap-4 items-center p-4 rounded-lg shadow-xl transition-shadow duration-300">
       <div className='mx-auto'>
         <Image
           src={news.image}
           alt={news.title}
           width={260}
           height={130}
-          className="rounded shadow-lg w-96 h-80 "
+          className="rounded shadow-xl w-full h-80 "
         />
       </div>
       
-      <div className='col-span-2 p-6 space-y-5'>
-        <h3 className='text-center font-bold text-5xl text-cyan-600'>{news.title}</h3>
-        <p  className='text-center text-3xl font-semibold '>{news.description}</p>
-        <button
-          className="text-3xl text-center text-pink-400 font-bold py-2 px-4 rounded"
-          onClick={readText} // استدعاء دالة readText عند الضغط
-        >
-        🔊  Read
-        </button>
+      <div className='md:col-span-2 p-6 space-y-3'>
+        <h3 className='text-center font-bold text-xl md:text-3xl text-seaBlue'>{news.title}</h3>
+        <p  className='text-center text-lg md:text-xl font-semibold '>{news.description}</p>
+      
       </div>
     </article>
   );
