24c58b7a8eed5d340fed455c376b1c251c6bf63a
AymanWahman <aymanwahman80@gmail.com>
2024-11-18 13:26:01 +0200

    size video

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,53 +1,48 @@
-"use client";
-// import Link from "next/link";
-import NewsCard from "@/components/NewsCard";
-import data from "@/components/dataNews";
+'use client';
 
-export default function StructureLetters() {
-  // دالة لتحويل النص إلى صوت
-  const readText = (text: string) => {
-    if (typeof window !== "undefined") {
-      const utterance = new SpeechSynthesisUtterance(text);
-      utterance.lang = "en-US"; // يمكنك تغيير اللغة هنا إذا لزم الأمر
-      const voices = window.speechSynthesis.getVoices();
-  
-      utterance.rate = 0.75; // سرعة الكلام
-      utterance.pitch = 1; // نغمة الصوت
-      utterance.voice = voices.find(voice => voice.name === 'Google UK English Female') || voices[2];
-      window.speechSynthesis.speak(utterance);
-    }
-  };
+import Image from 'next/image'
+import data from '@/components/DataNews'
+// import Link from 'next/link'
 
+export default function news() {
   return (
-    <div className="mt-28">
-  
+    <div className='mt-16'>
+    
+    {/* <div className='text-center font-bold text-2xl text-y'>
+  <Link href="/"><button className='text-xl hover:text-2xl md:text-4xl hover:md:text-5xl text-yellow-600 hover:text-gray-400 font-serif'>Home</button></Link>
+  </div> */}
+    
+    <section dir="rtl" className="py-4 px-4 text-center">
+        <h2 className="text-2xl font-bold">عن بورسعيد</h2>
+        <p className="mt-4 text-gray-600">بورسعيد مدينة ساحلية جميلة ذات تاريخ وثقافة غنية، تتميز بموقعها الجغرافي وتاريخها الرائع.</p>
+      </section>
+    {data && data.map((d) => (
+    <main key={d.id} className="grid grid-cols-1 md:grid-cols-3 mb-3 items-center justify-between gap-2">
 
-      <h2 className="text-center font-bold text-xl md:text-4xl my-4">
-        أحدث الأخبار
-      </h2>
-
-      {data &&
-        data.map((d) => (
-          <div key={d.id} className="p-4">
-            <NewsCard
-              letter={{
-                id: d.id,
-                title: d.Letter,
-                image: d.image,
-                trans: d.voice,
-                use: d.Word,
-                description: d.Sentence,
-                example: "",
-                exampleTranslation: "",
-                example1: "",
-                example1Translation: "",
-              }}
-              readText={() =>
-                readText(`${d.Letter}. ${d.Word}. ${d.Sentence}`)
-              } // تمرير دالة readText
-            />
-          </div>
-        ))}
+    <div className='my-5 ml-auto mr-auto'>
+    <Image
+      className="rounded shadow-2xl shadow-black w-64 h-56"
+      src={d.image}
+      alt="Logo"
+      width='260'
+      height='130'
+      priority
+    />
     </div>
-  );
+    
+    <div dir='rtl' className='col-span-2 p-3 md:mr-9 shadow-2xl shadow-black space-y-3 md:h-64 '>
+    <h1 className='text-center font-bold text-2xl md:text-4xl md:mt-8 text-seaBlue'>{d.title}</h1>
+    <p className='text-center text-lg md:text-xl font-bold '>{d.discription}</p>
+    <p className='text-center text-lg md:text-xl font-bold '>{d.history}</p>
+    </div>
+  
+  
+    
+    </main>
+   ))}
+
+
+  </div>
+  
+  )
 }
--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -2,32 +2,25 @@
 
 import Image from 'next/image';
 
-interface Letter {
+interface News {
   id: number;
   title: string;
-  image: string;
-  trans?: string; // اجعل trans اختيارية
-  use: string;
   description: string;
-  example: string;
-  exampleTranslation?: string; 
-  example1?: string;
-  example1Translation?: string; 
-  importance?: string;
+  image: string;
 }
 
-interface LetterCardProps {
-  letter: Letter; // الحقل letter من النوع Letter
+interface NewsCardProps {
+  news: News; // الحقل letter من النوع Letter
   readText?: () => void; // تمرير دالة readText
 }
 
-const NewsCard: React.FC<LetterCardProps> = ({ letter, readText }) => {
+const NewsCard: React.FC<NewsCardProps> = ({ news, readText }) => {
   return (
     <article className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center p-4 rounded shadow-md hover:shadow-lg transition-shadow duration-300">
       <div className='mx-auto'>
         <Image
-          src={letter.image}
-          alt={letter.title}
+          src={news.image}
+          alt={news.title}
           width={260}
           height={130}
           className="rounded shadow-lg w-96 h-80 "
@@ -35,12 +28,8 @@ const NewsCard: React.FC<LetterCardProps> = ({ letter, readText }) => {
       </div>
       
       <div className='col-span-2 p-6 space-y-5'>
-        <h3 className='text-center font-bold text-5xl text-cyan-600'>{letter.title}</h3>
-        <p dir='rtl' className='text-center text-3xl font-bold '>{letter.trans}</p>
-        <p dir='rtl' className='text-center text-3xl font-bold text-pink-400'>{letter.use}</p>
-        <p  className='text-center text-3xl font-semibold '>{letter.description}</p>
-        <p className='text-center text-3xl font-semibold text-cyan-600'>{letter.example}</p>
-        <p dir='rtl' className='text-center text-3xl font-semibold text-cyan-600'>{letter.exampleTranslation}</p>
+        <h3 className='text-center font-bold text-5xl text-cyan-600'>{news.title}</h3>
+        <p  className='text-center text-3xl font-semibold '>{news.description}</p>
         <button
           className="text-3xl text-center text-pink-400 font-bold py-2 px-4 rounded"
           onClick={readText} // استدعاء دالة readText عند الضغط
