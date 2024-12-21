41fcb0d604b2a1b11c0dacd4f9c0e3fb46fcdf97
AymanWahman <aymanwahman80@gmail.com>
2024-12-21 09:50:49 +0200

    fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,7 +20,7 @@ export default function Hero() {
             <h1 className="text-4xl md:text-6xl font-bold mb-7">
               مرحباً بكم في بورسعيد
             </h1>
-            <p className="text-xl md:text-2xl text-gray-500">
+            <p className="text-xl md:text-2xl ">
               عروس البحر المتوسط
             </p>
           </div>
