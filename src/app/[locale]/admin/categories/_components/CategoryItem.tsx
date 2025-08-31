c9703d6dd36beb25fe61c0da6480eb5e94001374
AymanWahman <aymanwahman80@gmail.com>
2025-08-31 13:58:19 +0300

    deploy2

--- src/app/[locale]/admin/categories/_components/CategoryItem.tsx
+++ src/app/[locale]/admin/categories/_components/CategoryItem.tsx
@@ -1,17 +1,17 @@
 import { Category } from "@prisma/client";
 import EditCategory from "./EditCategory";
-import { getCurrentLocale } from "@/lib/getCurrentLocale";
-import getTrans from "@/lib/translation";
+// import { getCurrentLocale } from "@/lib/getCurrentLocale";
+// import getTrans from "@/lib/translation";
 import DeleteCategory from "./DeleteCategory";
 
 async function CategoryItem({ category }: { category: Category }) {
-  const locale = await getCurrentLocale();
-  const translations = await getTrans(locale);
+  // const locale = await getCurrentLocale();
+  // const translations = await getTrans(locale);
   return (
     <li className="bg-gray-300 p-4 rounded-md flex justify-between">
       <h3 className="text-black font-medium text-lg flex-1">{category.name}</h3>
       <div className="flex items-center gap-2">
-        <EditCategory translations={translations} category={category} />
+        <EditCategory  category={category} />
         <DeleteCategory id={category.id} />
       </div>
     </li>
