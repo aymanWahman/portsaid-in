31ebfee4ba41ddf398bddaf3d9267ccc081fc931
AymanWahman <aymanwahman80@gmail.com>
2025-09-05 19:00:54 +0300

    Fix case sensitivity for translations.ts

---
+++ src/types/dataTypes.ts
@@ -0,0 +1,13 @@
+export type DataTypes = {
+  id: number;
+  name: string;
+  region: string;
+  address: string;
+  cuisine?: string[];
+  contact: string;
+  description: string;
+  coordinates?: [number, number];
+  image: string;
+  category: string;
+};
+
---
+++ src/types/newsTypes.ts
@@ -0,0 +1,8 @@
+export interface NewsTypes {
+  id: number;
+  title: string;
+  description: string;
+  image?: string;
+  history?: string;
+  content?: string;
+}
